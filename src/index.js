aad2edefbdf11663b4b812be37c5e5dc7c9aac6e
diegofernandodev <dieferv@gmail.com>
2024-04-22 16:13:45 -0500

    cambio en index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const appLittlebox = require("./config/server"); // Asegúrate de que la ruta sea correcta
 require("./config/DatabaseLittleBox"); // Asegúrate de que la ruta sea correcta
 
-appLittlebox.listen(appLittlebox.get("port"), ()=> {
+const HOST = '0.0.0.0'; // Esto hace que el servidor escuche en todas las interfaces de red disponibles
+
+appLittlebox.listen(appLittlebox.get("port"),HOST, ()=> {
     console.log('Server in running on port:', appLittlebox.get('port'))
 }); // Usa appLittlebox en lugar de serverLittlebox
